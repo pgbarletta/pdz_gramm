4282fe711ccfc20e710ec7cd93736dba939135b2
pgbarletta <pbarletta@gmail.com>
2017-03-31 14:04:19 -0300

    Added min_cost_assignment with limited width

--- analyze_qha_ca/lb_qha_post_ana.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Do Ligand Bound QHA post analysis
-/scratch/lfs/grosso/qha_programs/qha_post_analysis_prog/qha_ca_amber_analyze.exe lb_prod r 101 101 ca
--- analyze_qha_ca/lf_qha_post_ana.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-
-# Do Ligand Free QHA post analysis
-/scratch/lfs/grosso/qha_programs/qha_post_analysis_prog/qha_ca_amber_analyze.exe lf_prod r 101 101 ca
